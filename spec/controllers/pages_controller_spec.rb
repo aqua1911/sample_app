2dbf3fa10c4548594475f575ede093b9f354f402
Alexandre Savoie <alexandre.savoie@videogaminglounge.com>
2015-09-24 21:13:45 -0400

    Fin pages statiques

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,12 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe PagesController, type: :controller do
+  render_views
 
   describe "GET #home" do
     it "returns http success" do
       get :home
       expect(response).to have_http_status(:success)
     end
+	
+	# it "devrait avoir le bon titre" do
+      # get :home
+      # response.should have_selector("title",
+                        # :content => "Simple App du Tutoriel Ruby on Rails | Accueil")
+    # end
   end
 
   describe "GET #contact" do
@@ -14,6 +21,26 @@ RSpec.describe PagesController, type: :controller do
       get :contact
       expect(response).to have_http_status(:success)
     end
+	
+	# it "devrait avoir le bon titre" do
+      # get :contact
+      # response.should have_selector("title",
+                        # :content =>
+                          # "Simple App du Tutoriel Ruby on Rails | Contact")
+    # end
   end
 
+  describe "GET 'about'" do
+    it "returns http success" do
+      get 'about'
+      expect(response).to have_http_status(:success)
+    end
+	
+	# it "devrait avoir le bon titre" do
+      # get :about
+      # response.should have_selector("title",
+                        # :content =>
+                          # "Simple App du Tutoriel Ruby on Rails | A Propos")
+    # end
+  end
 end
